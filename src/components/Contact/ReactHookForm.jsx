4d7e9be9874d64b6e7d1818099c8ad19c2cb6a31
Farok Hossain <farokafs7@gmail.com>
2024-07-13 23:58:29 +0600

    updated hook form

--- src/components/Contact/ReactHookForm.jsx
+++ src/components/Contact/ReactHookForm.jsx
@@ -1,9 +1,19 @@
 const ReactHookForm = () => {
   return (
-    <form action="">
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button type="submit">Submit</button>
+    <form action="" className="mt-20">
+      <input
+        type="text"
+        placeholder="Email"
+        className="border border-green-600 border-b-orange-600 mr-3 rounded-md"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="border border-green-600 border-b-orange-600 mr-3 rounded-md"
+      />
+      <button className="btn btn-secondary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
